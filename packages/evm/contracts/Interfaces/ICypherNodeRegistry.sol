7ac26a4315b8c7fb2a060604c098af3afddfd0b0
Auryn Macmillan <hi@auryn.xyz>
2024-05-28 10:43:13 +1000

    add e3id to node selection

--- packages/evm/contracts/Interfaces/ICypherNodeRegistry.sol
+++ packages/evm/contracts/Interfaces/ICypherNodeRegistry.sol
@@ -3,8 +3,13 @@ pragma solidity >=0.8.26;
 
 interface ICypherNodeRegistry {
     /// @notice This function should be called by the Enclave contract to select a node committee.
+    /// @param e3Id ID of the E3 for which to select the committee.
     /// @param poolId ID of the pool of nodes from which to select the committee.
     /// @param threshold The M/N threshold for the committee.
     /// @return committeeId ID of the selected committee.
-    function selectCommittee(uint256 poolId, uint32[2] calldata threshold) external returns (bytes32 committeeId);
+    function selectCommittee(
+        uint256 e3Id,
+        uint256 poolId,
+        uint32[2] calldata threshold
+    ) external returns (bytes32 committeeId);
 }
