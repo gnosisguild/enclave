156da6f908e18344bdc2b565e4f7b0e2a181e14f
Auryn Macmillan <hi@auryn.xyz>
2024-06-06 23:39:41 +1000

    fix enclave deploy task

--- packages/evm/tasks/enclave.ts
+++ packages/evm/tasks/enclave.ts
@@ -2,6 +2,8 @@ import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
 task("task:deployEnclave", "Deploys Enclave contract")
+  .addParam("owner", "Account that will own this contract")
+  .addParam("registry", "Address of the cyphernode registry")
   .addParam("maxDuration", "The maximum duration of a computation in seconds")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
     const signers = await ethers.getSigners();
@@ -9,7 +11,11 @@ task("task:deployEnclave", "Deploys Enclave contract")
     console.log(`Deploying Enclave...`);
     const enclave = await enclaveFactory
       .connect(signers[0])
-      .deploy(taskArguments.maxDuration);
+      .deploy(
+        taskArguments.owner,
+        taskArguments.registry,
+        taskArguments.maxDuration,
+      );
     await enclave.waitForDeployment();
     console.log("Enclave deployed to: ", await enclave.getAddress());
   });
