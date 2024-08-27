64c50593bfdfefeb6ef563d5d07ef6b29de7ffda
гλ <contact@rudiyardley.com>
2024-08-28 09:30:04 +1000

    Add binaries for demonstrating Actor model flexability (#23)

---
+++ packages/ciphernode/enclave_node/src/bin/ciphernode-noag.rs
@@ -0,0 +1,21 @@
+use enclave_core::Actor;
+use enclave_core::Ciphernode;
+use enclave_core::Data;
+use enclave_core::EventBus;
+use enclave_core::Fhe;
+use enclave_core::P2p;
+use enclave_core::SimpleLogger;
+use std::error::Error;
+
+#[actix_rt::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let fhe = Fhe::try_default()?.start();
+    let bus = EventBus::new(true).start();
+    let data = Data::new(true).start(); // TODO: Use a sled backed Data Actor
+    SimpleLogger::attach(bus.clone());
+    Ciphernode::attach(bus.clone(), fhe.clone(), data.clone());
+    let (_, h) = P2p::spawn_libp2p(bus.clone())?;
+    println!("Ciphernode");
+    let _ = tokio::join!(h);
+    Ok(())
+}
