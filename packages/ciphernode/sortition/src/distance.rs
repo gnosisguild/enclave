34eb638a4f8b330f2618d25a23c30010de75d3a9
nginnever <ginneversource@gmail.com>
2024-09-10 13:16:14 -0700

    convert hash

---
+++ packages/ciphernode/sortition/src/distance.rs
@@ -0,0 +1,36 @@
+use alloy_primitives::{address, Address, keccak256};
+use num::{BigInt, Num};
+
+pub struct DistanceSortition {
+    pub random_seed: u64,
+    pub registered_nodes: Vec<Address>,
+    pub size: usize,
+}
+
+impl DistanceSortition {
+    pub fn new(random_seed: u64, registered_nodes: Vec<Address>, size: usize) -> Self {
+        Self { random_seed, registered_nodes, size }
+    }
+
+    pub fn get_committee(&mut self) -> Vec<Address> {
+        let hashed = self.registered_nodes.iter()
+            .map(|address|
+                {
+                    let concat = address.to_string() + &self.random_seed.to_string();
+                    let hash = keccak256(concat);
+                    hash.to_string()
+                })
+            .collect::<Vec<String>>();
+
+        let numeric = hashed.iter()
+            .map(|hash|
+                {
+                    let without_prefix = hash.trim_start_matches("0x");
+                    let z = BigInt::from_str_radix(without_prefix, 16);
+                    println!("{:?}", z);
+                    z.unwrap()
+                })
+            .collect::<Vec<BigInt>>();
+        self.registered_nodes.clone()
+    }
+}
