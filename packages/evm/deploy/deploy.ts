b930596d0073e3977cc36b35133c9be17bd8eb41
samepant <samepant@users.noreply.github.com>
2024-09-10 15:37:56 -0400

    add deploy script for deploying contracts, and setting registry address

--- packages/evm/deploy/deploy.ts
+++ packages/evm/deploy/deploy.ts
@@ -2,6 +2,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
+const addressOne = "0x0000000000000000000000000000000000000001";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
@@ -9,11 +10,41 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const enclave = await deploy("Enclave", {
     from: deployer,
-    args: [THIRTY_DAYS_IN_SECONDS],
+    args: [deployer, addressOne, THIRTY_DAYS_IN_SECONDS],
     log: true,
   });
 
   console.log(`Enclave contract: `, enclave.address);
+
+  const cypherNodeRegistry = await deploy("CyphernodeRegistryOwnable", {
+    from: deployer,
+    args: [deployer, enclave.address],
+    log: true,
+  });
+
+  console.log(
+    `CyphernodeRegistryOwnable contract: `,
+    cypherNodeRegistry.address,
+  );
+
+  // set registry in enclave
+  const enclaveContract = await hre.ethers.getContractAt(
+    "Enclave",
+    enclave.address,
+  );
+
+  const setRegistryAddress = await enclaveContract.cyphernodeRegistry();
+
+  if (setRegistryAddress === cypherNodeRegistry.address) {
+    console.log(`Enclave contract already has registry`);
+    return;
+  }
+
+  const result = await enclaveContract.setCyphernodeRegistry(
+    cypherNodeRegistry.address,
+  );
+  await result.wait();
+  console.log(`Enclave contract updated with registry`);
 };
 export default func;
 func.id = "deploy_enclave"; // id required to prevent reexecution
