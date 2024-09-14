aaeb6575b6d6dfda50137fa55d9e92f494ffe90d
ryardley <contact@rudiyardley.com>
2024-09-14 23:02:36 +1000

    Allow typechain to be run without env vars

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -10,14 +10,18 @@ import "./tasks/accounts";
 dotenv.config();
 
 const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY } = process.env;
-if (!INFURA_KEY || !MNEMONIC || !ETHERSCAN_API_KEY) {
-  console.error(
-    "Please set the INFURA_KEY, MNEMONIC, and ETHERSCAN_API_KEY environment variables",
-  );
-  process.exit(1);
+
+if (!process.env.TYPECHAIN) { // NOTE: enabling typechain compilation without ENV vars
+  if (!INFURA_KEY || !MNEMONIC || !ETHERSCAN_API_KEY) {
+    console.error(
+      "Please set the INFURA_KEY, MNEMONIC, and ETHERSCAN_API_KEY environment variables",
+    );
+    process.exit(1);
+  }
 }
-const mnemonic: string = MNEMONIC;
-const infuraApiKey: string = INFURA_KEY;
+
+const mnemonic: string = MNEMONIC || "";
+const infuraApiKey: string = INFURA_KEY || "";
 
 const chainIds = {
   "arbitrum-mainnet": 42161,
