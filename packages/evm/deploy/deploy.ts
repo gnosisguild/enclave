4d853a28c78e2f2b9f6e4c527da20222b12a31fa
samepant <samepant@users.noreply.github.com>
2024-09-16 14:59:49 -0400

    deploy public libraries if they don't exist on network

--- packages/evm/deploy/deploy.ts
+++ packages/evm/deploy/deploy.ts
@@ -1,5 +1,6 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { PoseidonT3, proxy } from "poseidon-solidity";
 
 const THIRTY_DAYS_IN_SECONDS = 60 * 60 * 24 * 30;
 const addressOne = "0x0000000000000000000000000000000000000001";
@@ -8,26 +9,58 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
+  // First check if the proxy exists
+  if ((await hre.ethers.provider.getCode(proxy.address)) === "0x") {
+    // probably on the hardhat network
+    // fund the keyless account
+    const [sender] = await hre.ethers.getSigners();
+    await sender.sendTransaction({
+      to: proxy.from,
+      value: proxy.gas,
+    });
+
+    // then send the presigned transaction deploying the proxy
+    await hre.ethers.provider.broadcastTransaction(proxy.tx);
+    console.log(`Proxy deployed to: ${proxy.address}`);
+  }
+
+  // Then deploy the hasher, if needed
+  if ((await hre.ethers.provider.getCode(PoseidonT3.address)) === "0x") {
+    const [sender] = await hre.ethers.getSigners();
+    await sender.sendTransaction({
+      to: proxy.address,
+      data: PoseidonT3.data,
+    });
+
+    console.log(`PoseidonT3 deployed to: ${PoseidonT3.address}`);
+  }
+
   // Deploy Enclave contract
 
   const enclave = await deploy("Enclave", {
     from: deployer,
     args: [deployer, addressOne, THIRTY_DAYS_IN_SECONDS],
     log: true,
+    libraries: {
+      PoseidonT3: PoseidonT3.address,
+    },
   });
 
   console.log(`Enclave contract: `, enclave.address);
 
-  // Deploy CyphernodeRegistryOwnable contract
+  // Deploy CiphernodeRegistryOwnable contract
 
-  const cypherNodeRegistry = await deploy("CyphernodeRegistryOwnable", {
+  const cypherNodeRegistry = await deploy("CiphernodeRegistryOwnable", {
     from: deployer,
     args: [deployer, enclave.address],
     log: true,
+    libraries: {
+      PoseidonT3: PoseidonT3.address,
+    },
   });
 
   console.log(
-    `CyphernodeRegistryOwnable contract: `,
+    `CiphernodeRegistryOwnable contract: `,
     cypherNodeRegistry.address,
   );
 
@@ -47,14 +80,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     enclave.address,
   );
 
-  const registryAddress = await enclaveContract.cyphernodeRegistry();
+  const registryAddress = await enclaveContract.ciphernodeRegistry();
 
   if (registryAddress === cypherNodeRegistry.address) {
     console.log(`Enclave contract already has registry`);
     return;
   }
 
-  const result = await enclaveContract.setCyphernodeRegistry(
+  const result = await enclaveContract.setCiphernodeRegistry(
     cypherNodeRegistry.address,
   );
   await result.wait();
