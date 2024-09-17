eaea34afa67e598aeb95907a2e607bdc1730481a
ryardley <contact@rudiyardley.com>
2024-09-17 16:20:10 +1000

    Remove comment

--- packages/ciphernode/core/src/evm.rs
+++ packages/ciphernode/core/src/evm.rs
@@ -85,37 +85,6 @@ impl Evm {
     }
 }
 
-// // Generic conversion for any type that implements SolEvent
-// impl<T> From<&T> for EnclaveEvent
-// where
-//     T: SolEvent + Send + Sync + 'static,
-// {
-//     fn from(event: &T) -> Self {
-//         // Use type_name to get the name of the event type
-//         let type_name = std::any::type_name::<T>();
-//         match type_name {
-//             "CommitteeRequested" => From::from(event as &CommitteeRequested),
-//             "CiphernodeAdded" => From::from(event as &CiphernodeAdded),
-//             "CiphernodeRemoved" => From::from(event as &CiphernodeRemoved),
-//             "CiphertextOutputPublished" => From::from(event as &CiphertextOutputPublished),
-//             _ => panic!("Unsupported event type: {}", type_name),
-//         }
-//     }
-// }
-//
-// impl From<&CommitteeRequested> for EnclaveEvent {
-//     fn from(value: CommitteeRequested) -> Self {
-//         EnclaveEvent::from(crate::events::CommitteeRequested {
-//             e3_id: E3id::from(value.e3Id),
-//             nodecount: value.threshold[1] as usize,
-//             sortition_seed: thread_rng().next_u64(),
-//             // HACK: set params here for event
-//             // TODO: pass params with committee requested event
-//             crp: Com,
-//         })
-//     }
-// }
-//
 impl Actor for Evm {
     type Context = Context<Self>;
 }
