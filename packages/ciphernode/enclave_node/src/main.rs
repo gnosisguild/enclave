e3ef5853f5b708a755b2b1d84aafaa6c1bef946e
nginnever <ginneversource@gmail.com>
2024-09-17 09:35:42 -0700

    p2p messages

--- packages/ciphernode/enclave_node/src/main.rs
+++ packages/ciphernode/enclave_node/src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
+use std::{str};
 
-use p2p::EnclaveRouter;
+use p2p::{EnclaveRouter, P2PMessage};
 use bfv::EnclaveBFV;
 use sortition::DistanceSortition;
 use tokio::{
@@ -9,6 +10,8 @@ use tokio::{
 };
 
 use alloy_primitives::{address};
+
+use log::Level;
 
 const OWO: &str = r#"
       ___           ___           ___                         ___                         ___     
@@ -25,8 +28,53 @@ const OWO: &str = r#"
                                                                       
 "#;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+fn handle_p2p_msg(msg: Vec<u8>) {
+    let msg_out_str = str::from_utf8(&msg).unwrap();
+    let msg_out_struct: P2PMessage = serde_json::from_str(&msg_out_str).unwrap();
+    println!("msg_topic: {}", msg_out_struct.topic);
+    println!("msg_type: {}", msg_out_struct.msg_type);
+    println!("msg: {}", String::from_utf8(msg_out_struct.data).unwrap());
+}
+
+async fn start_p2p() -> Result<(), Box<dyn Error>> {
+    log::info!("Connecting to swarm");
+    let (mut p2p, p2p_tx, mut p2p_rx) = EnclaveRouter::new()?;
+    p2p.connect_swarm("mdns".to_string())?;
+    p2p.join_topic("enclave-testnet")?;
+    let mut stdin = BufReader::new(io::stdin()).lines();
+    tokio::spawn(async move { p2p.start().await });
+    tokio::spawn(async move {
+        while let Some(msg) = p2p_rx.recv().await {
+            handle_p2p_msg(msg);
+        }
+    });
+    loop {
+        if let Ok(Some(line)) = stdin.next_line().await {
+            let msg_formatted = P2PMessage {
+                topic: "enclave-testnet".to_string(),
+                msg_type: "join_main_channel".to_string(),
+                data: line.into_bytes(),
+            };
+            let msg_str = serde_json::to_string(&msg_formatted).unwrap();
+            let msg_bytes = msg_str.into_bytes();
+            p2p_tx.send(msg_bytes.clone()).await.unwrap();
+        }
+    }
+}
+
+async fn start_eth_listener() {
+    log::info!("Listening on E3 Contract");
+}
+
+async fn run() {
+    tokio::join!(
+        start_p2p(),
+        start_eth_listener(),
+    );
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
     // boot up p2p network
 
     // boot up ether client
@@ -36,9 +84,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
     //let ether = eth::EtherClient::new("test".to_string());
     println!("\n\n\n\n\n{}", OWO);
     println!("\n\n\n\n");
-    println!("Hello, cipher world!");
 
-    let mut committee = DistanceSortition::new(12, vec![address!("d8da6bf26964af9d7eed9e03e53415d37aa96045")], 10);
+    simple_logger::init_with_level(Level::Info).unwrap();
+
+    let main_rt = tokio::runtime::Runtime::new().unwrap();
+    let future = run();
+    main_rt.block_on(future);
+
+    let mut committee = DistanceSortition::new(12, vec![address!("d8da6bf26964af9d7eed9e03e53415d37aa96045")], 1);
     committee.get_committee();
 
     let mut new_bfv = EnclaveBFV::new(4096, 4096, vec![0xffffee001, 0xffffc4001, 0x1ffffe0001]);
@@ -47,19 +100,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let crp_bytes = new_bfv.serialize_crp();
     let deserialized_pk = new_bfv.deserialize_pk(pk_bytes, param_bytes, crp_bytes);
 
-    let (mut p2p, tx, mut rx) = EnclaveRouter::new()?;
-    p2p.connect_swarm("mdns".to_string())?;
-    p2p.join_topic("enclave-keygen-01")?;
-    let mut stdin = BufReader::new(io::stdin()).lines();
-    tokio::spawn(async move { p2p.start().await });
-    tokio::spawn(async move {
-        while let Some(msg) = rx.recv().await {
-            println!("msg: {}", String::from_utf8(msg).unwrap());
-        }
-    });
-    loop {
-        if let Ok(Some(line)) = stdin.next_line().await {
-            tx.send(line.as_bytes().to_vec().clone()).await.unwrap();
-        }
-    }
+    Ok(())
 }
