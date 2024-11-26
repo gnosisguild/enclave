fc3dc0579bf484484b08090e96660217d0d130e0
ryardley <contact@rudiyardley.com>
2024-11-26 20:44:51 +0700

    Add Persistable Data to Keyshare

--- packages/ciphernode/keyshare/src/repositories.rs
+++ packages/ciphernode/keyshare/src/repositories.rs
@@ -2,14 +2,12 @@ use config::StoreKeys;
 use data::{Repositories, Repository};
 use enclave_core::E3id;
 
-use crate::KeyshareState;
-
 pub trait KeyshareRepositoryFactory {
-    fn keyshare(&self, e3_id: &E3id) -> Repository<KeyshareState>;
+    fn keyshare(&self, e3_id: &E3id) -> Repository<Vec<u8>>;
 }
 
 impl KeyshareRepositoryFactory for Repositories {
-    fn keyshare(&self, e3_id: &E3id) -> Repository<KeyshareState> {
+    fn keyshare(&self, e3_id: &E3id) -> Repository<Vec<u8>> {
         Repository::new(self.store.scope(StoreKeys::keyshare(e3_id)))
     }
 }
