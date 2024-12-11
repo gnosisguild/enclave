79d8d64e3ce5259ef0aad00477d0246d163fb8d9
ryardley <contact@rudiyardley.com>
2024-12-11 21:03:43 +1100

    Try using the --env-file arg

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -5,9 +5,4 @@ if [ ! -f "./.deploy/.env" ]; then
     exit 1
 fi
 
-# Source the environment file
-set -a  # automatically export all variables
-source .deploy/.env
-set +a  # turn off auto-export
-
-docker stack deploy -c .deploy/docker-compose.yml enclave-stack --detach=false
+docker stack deploy -c .deploy/docker-compose.yml --env-file .deploy/.env enclave-stack --detach=false
