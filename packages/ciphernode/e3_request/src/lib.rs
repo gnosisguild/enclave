4d5d0a3aa10fa829fb41bed849c8e4def6c3481d
ryardley <contact@rudiyardley.com>
2024-12-29 19:48:00 +1100

    Rename file

--- packages/ciphernode/e3_request/src/lib.rs
+++ packages/ciphernode/e3_request/src/lib.rs
@@ -1,11 +1,11 @@
-mod committee_meta;
 mod context;
 mod hetrogenous_map;
+mod meta;
 mod repo;
 mod router;
 
-pub use committee_meta::*;
 pub use context::*;
 pub use hetrogenous_map::*;
+pub use meta::*;
 pub use repo::*;
 pub use router::*;
