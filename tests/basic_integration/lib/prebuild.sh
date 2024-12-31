cba51d9a20c2b9503b885e61f6bc3ed9c950ed45
ryardley <contact@rudiyardley.com>
2024-12-31 13:51:26 +1100

    Update warnings

--- tests/basic_integration/lib/prebuild.sh
+++ tests/basic_integration/lib/prebuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-cd packages/ciphernode && RUSTFLAGS="-A warnings" cargo build --release --bin fake_encrypt --bin enclave --bin pack_e3_params;
+cd packages/ciphernode && cargo build --bin fake_encrypt --bin enclave --bin pack_e3_params;
