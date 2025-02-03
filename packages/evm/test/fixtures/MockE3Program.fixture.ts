30d1def5130fcf5d7cc1ef2d399c3c5cb721c629
ryardley <contact@rudiyardley.com>
2025-02-03 12:58:31 +0800

    Refactor and update input validation logic and tests in Enclave contract

--- packages/evm/test/fixtures/MockE3Program.fixture.ts
+++ packages/evm/test/fixtures/MockE3Program.fixture.ts
@@ -1,11 +1,12 @@
 import { ethers } from "hardhat";
 
+import { InputValidatorPolicy__factory } from "../../types/factories/contracts/InputValidatorPolicy__factory";
 import { MockE3Program__factory } from "../../types/factories/contracts/test/MockE3Program__factory";
 
 export async function deployE3ProgramFixture(inputValidatorAddress: string) {
   const deployment = await (
     await ethers.getContractFactory("MockE3Program")
   ).deploy(inputValidatorAddress);
-
-  return MockE3Program__factory.connect(await deployment.getAddress());
+  const deploymentAddress = await deployment.getAddress();
+  return MockE3Program__factory.connect(deploymentAddress);
 }
