727bbe41ef68f52276269ec68e529f4e4fd8c4ec
ryardley <contact@rudiyardley.com>
2025-02-04 19:06:43 +0800

    Simplify import statements by removing unused `CheckStatus` from multiple contracts

--- packages/evm/contracts/interfaces/IEnclavePolicy.sol
+++ packages/evm/contracts/interfaces/IEnclavePolicy.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.27;
 import {
     IAdvancedPolicy
 } from "../excubiae/core/interfaces/IAdvancedPolicy.sol";
-import { CheckStatus, Check } from "../excubiae/core/AdvancedChecker.sol";
+import { Check } from "../excubiae/core/AdvancedChecker.sol";
 
 /// @title IEnclavePolicy.
 /// @notice Extends IPolicy with multi-phase validation capabilities.
