36f62fd406cc27ec5a25b970c58e7fcd2b631e28
ryardley <contact@rudiyardley.com>
2025-03-28 17:34:21 +0700

    Linting

--- packages/evm/contracts/interfaces/IEnclavePolicy.sol
+++ packages/evm/contracts/interfaces/IEnclavePolicy.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.27;
 
+// solhint-disable no-empty-blocks
+
 import {
     IAdvancedPolicy
 } from "@excubiae/contracts/interfaces/IAdvancedPolicy.sol";
-import { Check } from "@excubiae/contracts/checker/AdvancedChecker.sol";
 
 /// @title IEnclavePolicy.
 /// @notice Extends IPolicy with multi-phase validation capabilities.
-interface IEnclavePolicy is IAdvancedPolicy {
-
-}
+interface IEnclavePolicy is IAdvancedPolicy {}
