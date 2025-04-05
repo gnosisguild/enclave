29360ab9f66a94e658f703b268392900b3fb8d81
ryardley <contact@rudiyardley.com>
2025-04-05 18:45:43 +0700

    Update dependencies and configure OpenTelemetry with OTLP and stdout exporters

--- packages/ciphernode/enclave/src/helpers/telemetry.rs
+++ packages/ciphernode/enclave/src/helpers/telemetry.rs
@@ -1,8 +1,26 @@
 use anyhow::Result;
 use config::AppConfig;
+use opentelemetry::trace::{Tracer, TracerProvider as _};
+use opentelemetry_sdk::trace::SdkTracerProvider;
 use tracing::Level;
-
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 pub fn setup_tracing(_config: &AppConfig, log_level: Level) -> Result<()> {
-    tracing_subscriber::fmt().with_max_level(log_level).init();
+    // Initialize OTLP exporter using gRPC (Tonic)
+    let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .build()?;
+    let provider = SdkTracerProvider::builder()
+        .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+        .build();
+    let _ = opentelemetry_sdk::trace::SdkTracerProvider::builder()
+        .with_batch_exporter(otlp_exporter)
+        .build();
+
+    let tracer = provider.tracer("readme_example");
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry().with(telemetry).init();
+
     Ok(())
 }
