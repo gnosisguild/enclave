de5c95e149d753cf7aaed7f22856ef336ec36d06
ryardley <contact@rudiyardley.com>
2025-04-09 18:36:46 +0700

    Update git submodule initialization to include `--init --recursive` in setup script

--- examples/CRISP/scripts/setup.sh
+++ examples/CRISP/scripts/setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # This is all stuff that has to happen after the source code is mounted 
 # TOOD: perhaps we can try and move more of this to the dockerfile build process
 # Eg. copy package.json and Cargo.toml and then try to build out dependencies however this is relatively complex
-(cd /app && git submodule update)
+(cd /app && git submodule update --init --recursive)
 (cd /app && find . -name "node_modules" -type d -prune -exec rm -rf {} + && pnpm install)
 echo "evm"
 (cd /app/packages/evm && pnpm compile)
