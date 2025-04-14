11ac50af532d70259b76d8d89a9ed477b311641f
Hamza Khalid <hamzakhalid829@gmail.com>
2025-04-14 17:54:37 +0500

    Move Server & Client to apps

--- examples/CRISP/risc0/apps/risc0/methods/build.rs
+++ examples/CRISP/risc0/apps/risc0/methods/build.rs
@@ -18,8 +18,8 @@ use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptions
 use risc0_build_ethereum::generate_solidity_files;
 
 // Paths where the generated Solidity files will be written.
-const SOLIDITY_IMAGE_ID_PATH: &str = "../contracts/ImageID.sol";
-const SOLIDITY_ELF_PATH: &str = "../tests/Elf.sol";
+const SOLIDITY_IMAGE_ID_PATH: &str = "../../../contracts/ImageID.sol";
+const SOLIDITY_ELF_PATH: &str = "../../../tests/Elf.sol";
 
 fn main() {
     // Builds can be made deterministic, and thereby reproducible, by using Docker to build the
@@ -44,6 +44,6 @@ fn main() {
     let solidity_opts = risc0_build_ethereum::Options::default()
         .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
         .with_elf_sol_path(SOLIDITY_ELF_PATH);
-
+    
     generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
 }
