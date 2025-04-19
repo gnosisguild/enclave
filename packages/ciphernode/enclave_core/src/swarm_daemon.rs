65e49d1fd431fefd3543fddeb01aeb71d3f0d870
ryardley <contact@rudiyardley.com>
2025-04-19 16:11:00 +0700

    Refactor to struct

--- packages/ciphernode/enclave_core/src/swarm_daemon.rs
+++ packages/ciphernode/enclave_core/src/swarm_daemon.rs
@@ -2,20 +2,20 @@ use anyhow::*;
 use config::{combine_unique, AppConfig, NodeDefinition};
 use std::io::Write;
 use std::sync::Arc;
-use std::time::Duration;
 use std::{collections::HashMap, env};
 use tokio::io::AsyncBufReadExt;
 use tokio::process::{Child, ChildStderr, ChildStdout};
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
-use tokio::time::sleep;
 use tracing::{error, info, instrument};
 
 use crate::helpers::swarm::spawn_process;
 
 /// All the parameters of a command
 type CommandParams = (String, Vec<String>);
+/// A map of all the start commands to manage
+type CommandMap = HashMap<String, CommandParams>;
 /// The management record of the individual process
 type ProcessRecord = (Child, Vec<JoinHandle<()>>);
 /// The map that holds processes
@@ -89,7 +89,7 @@ fn extract_commands(
     exclude: Vec<String>,
     verbose: u8,
     maybe_config_string: Option<String>,
-) -> Result<HashMap<String, CommandParams>> {
+) -> Result<CommandMap> {
     let mut exclude_list = exclude.clone();
 
     // Default should not be part of swarm
@@ -171,9 +171,8 @@ async fn run_command(id: &str, program: &str, args: Vec<String>) -> Result<Proce
 }
 
 /// Run commands as child processes and set up output forwarding
-async fn run_commands(commands: HashMap<String, CommandParams>) -> Result<ProcessMap> {
-    let processes = Arc::new(Mutex::new(HashMap::new()));
-
+async fn run_commands(commands: &CommandMap, processes: &ProcessMap) -> Result<()> {
+    let commands = commands.clone();
     for (id, (program, args)) in commands {
         let record = run_command(&id, &program, args).await?;
 
@@ -181,10 +180,40 @@ async fn run_commands(commands: HashMap<String, CommandParams>) -> Result<Proces
         let mut processes_guard = processes.lock().await;
         processes_guard.insert(id, record);
     }
-
-    Ok(processes)
+    Ok(())
 }
 
+/// Start a process
+async fn start(id: &str, commands: &CommandMap, processes: &ProcessMap) -> Result<()> {
+    if processes.lock().await.contains_key(id) {
+        bail!("Process {} already running!", id);
+    }
+    let Some(command) = commands.get(id) else {
+        bail!("Bad command {}", id);
+    };
+
+    let (program, args) = command.clone();
+    let record = run_command(id, &program, args).await?;
+    let mut processes_guard = processes.lock().await;
+    processes_guard.insert(id.to_owned(), record);
+
+    Ok(())
+}
+
+/// Start a process
+async fn stop(id: &str, processes: &ProcessMap) -> Result<()> {
+    let mut processes_lock = processes.lock().await;
+    if !processes_lock.contains_key(id) {
+        info!("Cannot stop process that isn't running {}", id);
+        return Ok(());
+    };
+    if let Some(mut process_record) = processes_lock.get_mut(id) {
+        terminate_process_record(id, &mut process_record).await;
+    }
+    Ok(())
+}
+
+/// Terminate a process
 async fn terminate_process_record(id: &str, process_record: &mut ProcessRecord) {
     let (child, handlers) = process_record;
     for handler in handlers.drain(..) {
@@ -201,29 +230,98 @@ async fn terminate_process_record(id: &str, process_record: &mut ProcessRecord) 
     info!("Process {} terminated.", id);
 }
 
-/// Terminate all child processes
-async fn terminate_processes_and_exit(processes: ProcessMap) {
-    let mut processes_guard = processes.lock().await;
+/// Terminate all processes
+async fn terminate_processes(processes: &ProcessMap) {
     info!("starting to terminate processes...");
-    for (id, process_record) in processes_guard.iter_mut() {
-        terminate_process_record(id, process_record).await
+    let keys: Vec<String> = {
+        processes
+            .lock()
+            .await
+            .keys()
+            .map(|k| k.to_string())
+            .collect()
+    };
+
+    let mut processes_guard = processes.lock().await;
+    for id in keys {
+        if let Some(mut process_record) = processes_guard.remove(&id) {
+            terminate_process_record(&id, &mut process_record).await;
+        }
     }
+}
+
+/// Terminate all child processes
+async fn terminate_processes_and_exit(processes: &ProcessMap) {
+    terminate_processes(processes).await;
     info!("SWARM All processes terminated, exiting");
     let _ = std::io::stdout().flush();
     std::process::exit(0);
 }
 
 /// Set up signal handlers for graceful shutdown
-fn setup_signal_handlers(processes: ProcessMap) -> JoinHandle<()> {
-    let processes_term = processes.clone();
+fn setup_signal_handlers(manager: &ProcessManager) -> JoinHandle<()> {
+    let manager = manager.clone();
     tokio::spawn(async move {
         let mut sigterm =
             signal(SignalKind::terminate()).expect("SWARM Failed to set up SIGTERM handler");
         sigterm.recv().await;
 
         info!("Received SIGTERM, shutting down all processes...");
-        terminate_processes_and_exit(processes_term).await;
+        manager.terminate().await
     })
+}
+
+#[derive(Debug, Clone)]
+struct ProcessManager {
+    commands: CommandMap,
+    processes: ProcessMap,
+}
+
+impl ProcessManager {
+    pub async fn start_all(&self) -> Result<()> {
+        run_commands(&self.commands, &self.processes).await?;
+        Ok(())
+    }
+
+    pub async fn start(&self, id: &str) -> Result<()> {
+        start(id, &self.commands, &self.processes).await?;
+        Ok(())
+    }
+
+    pub async fn stop(&self, id: &str) -> Result<()> {
+        stop(id, &self.processes).await?;
+        Ok(())
+    }
+
+    pub async fn restart(&self, id: &str) -> Result<()> {
+        stop(id, &self.processes).await?;
+        start(id, &self.commands, &self.processes).await?;
+        Ok(())
+    }
+
+    pub async fn stop_all(&self) -> Result<()> {
+        terminate_processes(&self.processes).await;
+        Ok(())
+    }
+
+    pub async fn terminate(&self) {
+        terminate_processes_and_exit(&self.processes).await;
+    }
+}
+
+impl From<CommandMap> for ProcessManager {
+    fn from(value: CommandMap) -> Self {
+        // TODO: should probably implement a singleton pattern here but rn it doesn't matter
+        let processes = Arc::new(Mutex::new(HashMap::new()));
+        let manager = Self {
+            commands: value,
+            processes,
+        };
+
+        setup_signal_handlers(&manager);
+
+        manager
+    }
 }
 
 #[instrument(skip_all)]
@@ -234,15 +332,7 @@ pub async fn execute(
     verbose: u8,
     maybe_config_string: Option<String>,
 ) -> Result<()> {
-    // TODO: refactor to be a struct with an interface
-    // new (commands)
-    // restart (id)
-    // start (id)
-    // stop (id)
-    // start_all ()
-    // stop_all ()
-
-    let cmds = extract_commands(
+    let command_map = extract_commands(
         config.nodes(),
         "127.0.0.1",
         exclude,
@@ -250,13 +340,12 @@ pub async fn execute(
         maybe_config_string,
     )?;
 
-    let processes = run_commands(cmds).await?;
-
-    setup_signal_handlers(processes.clone());
+    let process_manager = ProcessManager::from(command_map);
+    process_manager.start_all().await?;
 
     tokio::signal::ctrl_c().await?;
 
     info!("SWARM: Received Ctrl+C shutting down...");
-    terminate_processes_and_exit(processes).await;
+    process_manager.terminate().await;
     Ok(())
 }
