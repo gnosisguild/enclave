aa7826651add1b5076d8f6662a1abb60886843fb
ryardley <contact@rudiyardley.com>
2025-04-25 21:09:54 +0700

    Remove redundant var

--- examples/CRISP/scripts/tasks/dev_cipher.sh
+++ examples/CRISP/scripts/tasks/dev_cipher.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-PASSWORD="This is a password"
 PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 enclave wallet set --name ag --private-key "$PRIVATE_KEY" 
