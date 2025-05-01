c5676a7c185d2a3d4ecf728d98fc5a6ab0a380dc
ryardley <contact@rudiyardley.com>
2025-05-01 18:51:32 +0700

    Fix issues with docker environment

--- examples/CRISP/scripts/tasks/build.sh
+++ examples/CRISP/scripts/tasks/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-(cd evm && pnpm compile)
+(cd /app/packages/evm && pnpm compile)
 (cd ./apps/wasm-crypto && cargo build)
 (cd ./apps/program && cargo build)
 (cd ./apps/server && cargo build)
