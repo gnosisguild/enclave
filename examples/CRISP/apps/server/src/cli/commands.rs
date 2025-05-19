7ebd07aa7489cb4ea5de4d4c5588e8b3be9bfe7c
ryardley <contact@rudiyardley.com>
2025-05-19 14:13:29 +0200

    fix isomorphic dep issue

--- examples/CRISP/apps/server/src/cli/commands.rs
+++ examples/CRISP/apps/server/src/cli/commands.rs
@@ -8,7 +8,7 @@ use super::{CLI_DB, CONFIG};
 use alloy::primitives::{Address, Bytes, U256};
 use crisp::server::config::CONFIG as SERVER_CONFIG; // TODO: unify configuration
 use enclave_sdk::evm::contracts::EnclaveContract;
-use enclave_sdk_isomorphic::bfv::{
+use enclave_sdk::bfv::{
     build_bfv_params_arc, encode_bfv_params, params::SET_2048_1032193_1,
 };
 use fhe_rs::bfv::{BfvParameters, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
