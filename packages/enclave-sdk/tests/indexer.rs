1a8443d377e568f964f49a07dfd34e757adf29fd
ryardley <contact@rudiyardley.com>
2025-05-21 14:39:16 +0200

    Update test

--- packages/enclave-sdk/tests/indexer.rs
+++ packages/enclave-sdk/tests/indexer.rs
@@ -30,46 +30,85 @@ async fn test_indexer() -> Result<()> {
 
     let indexer = EnclaveIndexer::new(&endpoint, &address, InMemoryStore::new()).await?;
 
+    // Start tracking state
     indexer.start()?;
 
     // E3Activated
     let e3_id = 10;
-    let expiration = 10;
-    let public_key = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let pubkey = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     contract
         .emitE3Activated(
             Uint::from(e3_id),
-            Uint::from(expiration),
-            Bytes::from(public_key),
+            Uint::from(10),
+            Bytes::from(pubkey.clone()),
         )
         .send()
         .await?
         .watch()
         .await?;
-
-    sleep(Duration::from_millis(1)).await;
 
     // InputPublished
     let data = "Random data that wont actually be a string".to_string();
-    let data_hash = 1234;
-    let index = 1;
     contract
         .emitInputPublished(
             Uint::from(e3_id),
-            Bytes::from(data),
-            Uint::from(data_hash),
-            Uint::from(index),
+            Bytes::from(data.clone()),
+            Uint::from(1111),
+            Uint::from(1),
         )
         .send()
         .await?
         .watch()
         .await?;
 
-    sleep(Duration::from_millis(1)).await;
+    contract
+        .emitInputPublished(
+            Uint::from(e3_id),
+            Bytes::from(data.clone()),
+            Uint::from(2222),
+            Uint::from(2),
+        )
+        .send()
+        .await?
+        .watch()
+        .await?;
+
+    contract
+        .emitInputPublished(
+            Uint::from(e3_id),
+            Bytes::from(data.clone()),
+            Uint::from(3333),
+            Uint::from(3),
+        )
+        .send()
+        .await?
+        .watch()
+        .await?;
+
+    sleep(Duration::from_millis(10)).await;
+
+    assert_eq!(indexer.get_e3(e3_id).await?.ciphertext_inputs.len(), 3);
+    assert_eq!(
+        indexer.get_e3(e3_id).await?.ciphertext_inputs,
+        vec![
+            (Bytes::from(data.clone()).to_vec(), 1),
+            (Bytes::from(data.clone()).to_vec(), 2),
+            (Bytes::from(data.clone()).to_vec(), 3),
+        ]
+    );
+
+    let ciphertext_output = vec![9, 8, 7, 6, 5, 4, 3, 2, 1];
+    contract
+        .emitCiphertextOutputPublished(Uint::from(e3_id), Bytes::from(ciphertext_output.clone()))
+        .send()
+        .await?
+        .watch()
+        .await?;
 
     let e3 = indexer.get_e3(e3_id).await?;
 
-    assert_eq!(e3.ciphertext_inputs.len(), 1);
+    assert_eq!(e3.ciphertext_output, ciphertext_output);
 
     Ok(())
 }
