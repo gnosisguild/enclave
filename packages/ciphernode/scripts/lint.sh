334d5636c12e3809263e6d3d7e4f35673085dbe3
ryardley <contact@rudiyardley.com>
2025-05-30 16:51:57 +0200

    Tidy up lint scripts

--- packages/ciphernode/scripts/lint.sh
+++ packages/ciphernode/scripts/lint.sh
@@ -6,4 +6,4 @@ pushd ./evm && ./scripts/build_fixtures.sh && popd
 pushd ./evm-helpers && ./scripts/build_fixtures.sh && popd
 pushd ./indexer && ./scripts/build_fixtures.sh && popd
 
-cargo test -- $@
+cargo fmt -- --check
