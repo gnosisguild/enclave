f92e891106c362ccb1d5f751cd459a34ba372983
ryardley <contact@rudiyardley.com>
2025-06-01 15:00:49 +0200

    Update linting

--- examples/basic/hardhat.config.ts
+++ examples/basic/hardhat.config.ts
@@ -1,7 +1,7 @@
+import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 import "@gnosis-guild/enclave/deploy/enclave";
-import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: {
