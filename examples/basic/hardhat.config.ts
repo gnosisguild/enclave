ba69aa17396c4acd51888d53e170821c1bce98bc
ryardley <contact@rudiyardley.com>
2025-06-01 13:10:37 +0200

    update hh config

--- examples/basic/hardhat.config.ts
+++ examples/basic/hardhat.config.ts
@@ -1,24 +1,11 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
-import type { HardhatUserConfig } from "hardhat/config";
 import "@gnosis-guild/enclave/deploy/enclave";
+import type { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "localhost",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
-  },
   solidity: {
     version: "0.8.27",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
-      viaIR: true,
-    },
     overrides: {
       "node_modules/poseidon-solidity/PoseidonT3.sol": {
         version: "0.7.0",
@@ -30,17 +17,6 @@ const config: HardhatUserConfig = {
         },
       },
     },
-  },
-  // Point to the contracts in the installed package
-  paths: {
-    sources: "./node_modules/@gnosis-guild/enclave/contracts",
-    artifacts: "./artifacts",
-    cache: "./cache",
-    tests: "./test",
-  },
-  typechain: {
-    outDir: "types",
-    target: "ethers-v6",
   },
 };
 
