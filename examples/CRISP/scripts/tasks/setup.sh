eeb3361ee8cbeff9d54a07c6b14628662bbc7541
ryardley <contact@rudiyardley.com>
2025-06-01 22:00:03 +0200

    Fix setup task

--- examples/CRISP/scripts/tasks/setup.sh
+++ examples/CRISP/scripts/tasks/setup.sh
@@ -12,7 +12,7 @@ echo "pnpm install"
 echo "evm"
 (cd /app/packages/evm && pnpm compile)
 echo "ciphernode"
-(cd /app && cargo build --bin enclave && cargo install --bin enclave --force)
+(cd /app && cargo build -p e3-cli && cargo install --path packages/ciphernode/cli)
 echo "program"
 (cd ./apps/program && cargo build --bin crisp-program)
 echo "server"
