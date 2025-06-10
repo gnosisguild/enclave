24feb98674f0ea2392b36aa6f661c891af0ed620
Hamza Khalid <hamzakhalid829@gmail.com>
2025-06-10 15:03:47 +0500

    Update Enclave Cli

--- crates/cli/src/program.rs
+++ crates/cli/src/program.rs
@@ -4,12 +4,16 @@ use e3_config::AppConfig;
 
 #[derive(Subcommand, Debug)]
 pub enum ProgramCommands {
+    /// Start the program
+    Start,
+
     /// Compile the program code
     Compile,
 }
 
-pub async fn execute(command: ProgramCommands, config: &AppConfig) -> Result<()> {
+pub async fn execute(command: ProgramCommands, _config: &AppConfig) -> Result<()> {
     match command {
+        ProgramCommands::Start => e3_support_scripts::program_start().await?,
         ProgramCommands::Compile => e3_support_scripts::program_compile().await?,
     };
 
