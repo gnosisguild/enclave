745f241bf30991489403ce0db15c3608f3bdc10c
ryardley <contact@rudiyardley.com>
2025-06-10 11:16:31 +0100

    Remove redundant args

--- templates/default/scripts/dev_program.sh
+++ templates/default/scripts/dev_program.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 pnpm wait-on http://localhost:8545 && \
   concurrently -r \
     "pnpm dev:server" \
-    "enclave program start --json-rpc-server http://localhost:8080 --chain hardhat"
+    "enclave program start"
