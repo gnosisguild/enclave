4ec479d48121da2c2654d1cbfc801456e3f7656b
гλ <contact@rudiyardley.com>
2025-06-11 22:44:49 +0000

    Args in support scripts (#504)
    * Update script
    
    * Update inner script

--- crates/support/scripts/container/start.sh
+++ crates/support/scripts/container/start.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
-while [[ $# -gt 0 ]]; do
-    if [[ $1 == --api-key ]]; then
-        export BONSAI_API_KEY="$2"
-        shift 2
-    elif [[ $1 == --api-url ]]; then
-        export BONSAI_API_URL="$2"
-        shift 2
-    else
-        break
-    fi
-done
 
-[[ -z "$BONSAI_API_KEY" ]] && export RISC0_DEV_MODE=1
+# Clear any existing environment variables
+unset BONSAI_API_KEY BONSAI_API_URL
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --api-key)
+      export BONSAI_API_KEY="$2"
+      export RISC0_DEV_MODE=1
+      shift 2
+      ;;
+    --api-url)
+      export BONSAI_API_URL="$2"
+      unset RISC0_DEV_MODE
+      shift 2
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
 
 exec cargo run --bin e3-support-app "$@"
