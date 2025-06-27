78a1c07e30843645df93038fd0486456209fec2f
Hamza Khalid <36852564+hmzakhalid@users.noreply.github.com>
2025-06-27 13:52:21 +0500

    Switch to Semaphore Noir (#557)
    Update Dockerfile
    
    Add Missing Packages
    
    Use Correct Addresses

--- examples/CRISP/apps/client/src/utils/proof-encoding.ts
+++ examples/CRISP/apps/client/src/utils/proof-encoding.ts
@@ -1,26 +1,22 @@
-import { hexToBytes, encodeAbiParameters, parseAbiParameters } from 'viem';
-import { type SemaphoreProof } from '@semaphore-protocol/core';
+import { hexToBytes, encodeAbiParameters, parseAbiParameters, bytesToHex } from 'viem';
+import { type SemaphoreNoirProof } from '@semaphore-protocol/core';
 
 const abi = parseAbiParameters(
-    'uint256,uint256,uint256,uint256,uint256,uint256[8]'
+    '(uint256,uint256,uint256,uint256,uint256,bytes)'
 );
 
-type Tuple8<T> = readonly [T, T, T, T, T, T, T, T];
-
 export function encodeSemaphoreProof(
-    { merkleTreeDepth, merkleTreeRoot, nullifier, message, scope, points }: SemaphoreProof
+    { merkleTreeDepth, merkleTreeRoot, nullifier, message, scope, proofBytes }: SemaphoreNoirProof
 ): Uint8Array {
-    if (points.length !== 8) {
-        throw new Error('Semaphore proof must have 8 points');
-    }
-
     const hex = encodeAbiParameters(abi, [
-        BigInt(merkleTreeDepth),
-        BigInt(merkleTreeRoot),
-        BigInt(nullifier),
-        BigInt(message),
-        BigInt(scope),
-        points.map(BigInt) as unknown as Tuple8<bigint>,
+        [
+            BigInt(merkleTreeDepth),
+            BigInt(merkleTreeRoot),
+            BigInt(nullifier),
+            BigInt(message),
+            BigInt(scope),
+            bytesToHex(proofBytes),
+        ]
     ]);
 
     return hexToBytes(hex);
