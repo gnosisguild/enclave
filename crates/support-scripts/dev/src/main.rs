57d99256aa2fd435be6b1896e270878ad4ff21dc
ryardley <contact@rudiyardley.com>
2025-07-09 20:13:05 +0400

    Update e3 program server

--- crates/support-scripts/dev/src/main.rs
+++ crates/support-scripts/dev/src/main.rs
@@ -1,5 +1,10 @@
-use e3_program_server;
+use anyhow::Result;
+use e3_program_server::E3ProgramServer;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
+    let server = E3ProgramServer::builder(move |_| async { Ok((vec![], vec![])) }).build();
+    server.run().await?;
     println!("Hello, world!");
+    Ok(())
 }
