906dae86128d64918e32c051cc37eb7b994275d6
ryardley <contact@rudiyardley.com>
2025-07-10 13:33:57 +0400

    Add debugging

--- crates/support-scripts/src/program.rs
+++ crates/support-scripts/src/program.rs
@@ -7,9 +7,13 @@ use crate::{
 };
 
 fn get_mode(config: ProgramConfig, mode: Option<bool>) -> bool {
+    println!("*************************************************************");
     if let Some(m) = mode {
+        println!("USING PASSED IN MODE! WHICH IS {}", m);
         return m;
     };
+    println!("USING CONFIG MODE! WHICH IS {}", config.dev());
+
     config.dev()
 }
 
--- templates/default/scripts/dev_program.sh
+++ templates/default/scripts/dev_program.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
+echo "enclave rev = $(enclave rev)"
 echo "Waiting on ciphernodes to be ready..."
 pnpm wait-on file:/tmp/enclave_ciphernodes_ready
 echo "Ciphernodes are ready!"
