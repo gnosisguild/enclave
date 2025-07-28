baa48a25d916010c8cb21e10cb5a02d48f74a9b1
ryardley <contact@rudiyardley.com>
2025-07-29 00:21:20 +0400

    feat: add header

--- crates/doclinker/src/main.rs
+++ crates/doclinker/src/main.rs
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
 use clap::{Arg, Command};
 use regex::Regex;
 use std::collections::HashMap;
