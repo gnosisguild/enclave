4fbb16fee2f75bfc37d7adbc194e316bcdb67b5b
ryardley <contact@rudiyardley.com>
2025-08-05 19:29:58 +0400

    feat: event definition with stages

--- crates/events/src/enclave_event/compute_request.rs
+++ crates/events/src/enclave_event/compute_request.rs
@@ -11,38 +11,70 @@ use actix::Message;
 use e3_crypto::SensitiveBytes;
 use serde::{Deserialize, Serialize};
 
+/// Reference count bytes so event can be cloned and shared between threads
 pub type Bytes = Arc<Vec<u8>>;
 
-///
+/// Input format for TrBFVRequest
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum TrBFVRequest {
     GenerateEsiShares {
+        params: Bytes,
         num_summed: u64,
     },
-    GenerateSecretShares,
+    GenerateSecretShares {
+        params: Bytes,
+    },
     Decrypt {
+        params: Bytes,
         d_share_polys: Vec<Bytes>,
     },
+    GenerateDecryptionKey {
+        sk_sss_collected: Vec<SensitiveBytes>,
+        esi_sss_collected: Vec<SensitiveBytes>,
+    },
     GenerateDecryptionShare {
+        params: Bytes,
         ciphertext: Bytes,
         sk_poly_sum: SensitiveBytes,
         es_poly_sum: SensitiveBytes,
     },
+    ThresholdDecrypt {
+        d_share_polys: Vec<SensitiveBytes>,
+    },
 }
 
+/// Result format for TrBFVRequest
 pub enum TrBFVResult {
-    GenerateEsiShares {},
+    GenerateEsiShares {
+        esi_sss: Vec<SensitiveBytes>,
+    },
+    GenerateSecretShares {
+        pk_share: Bytes,
+        sk_sss: Vec<SensitiveBytes>,
+    },
+    GenerateDecryptionKey {
+        sk_poly_sum: SensitiveBytes,
+        es_poly_sum: SensitiveBytes,
+    },
+    GenerateDecryptionShare {
+        d_share_poly: Bytes,
+    },
+    ThresholdDecrypt {
+        result: Bytes,
+    },
 }
 
 /// The compute instruction for a threadpool computation
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub enum ComputeRequest {}
+pub enum ComputeRequest {
+    TrBFV(TrBFVRequest),
+}
 
 #[derive(Message, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[rtype(result = "()")]
 pub struct ComputeRequested {
     correlation_id: CorrelationId,
     instruction: ComputeRequest,
-    input: SensitiveBytes,
 }
 
 #[derive(Message, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
