d77c7faeca93538e0154f4c6de256e3ab8108452
Hamza Khalid <hamzakhalid829@gmail.com>
2025-08-05 10:35:29 +0500

    feat(token): add access control

--- packages/evm/contracts/tokenomics/Token.sol
+++ packages/evm/contracts/tokenomics/Token.sol
@@ -2,16 +2,24 @@
 pragma solidity >=0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract EnclaveToken is ERC20 {
+contract EnclaveToken is ERC20, AccessControl {
     uint256 public constant TOTAL_SUPPLY = 1_200_000_000e18;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     uint256 public totalMinted;
 
-    constructor() ERC20("Enclave", "ENCL") {
+    constructor(address _owner) ERC20("Enclave", "ENCL") {
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
+        _grantRole(MINTER_ROLE, _owner);
         totalMinted = 0;
     }
 
-    function mint(address recipient, uint256 amount) external {
+    function mint(
+        address recipient,
+        uint256 amount
+    ) external onlyRole(MINTER_ROLE) {
         require(totalMinted + amount <= TOTAL_SUPPLY, "Exceeds cap");
         _mint(recipient, amount);
         totalMinted += amount;
