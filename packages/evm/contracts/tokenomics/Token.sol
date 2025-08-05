0a9520892c0d9ee7b77fccd45239a54578077373
Hamza Khalid <hamzakhalid829@gmail.com>
2025-08-05 10:17:46 +0500

    feat(token): add total cap and tracking

--- packages/evm/contracts/tokenomics/Token.sol
+++ packages/evm/contracts/tokenomics/Token.sol
@@ -4,5 +4,16 @@ pragma solidity >=0.8.27;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EnclaveToken is ERC20 {
-    constructor() ERC20("Enclave", "ENCL") {}
+    uint256 public constant TOTAL_SUPPLY = 1_200_000_000e18;
+    uint256 public totalMinted;
+
+    constructor() ERC20("Enclave", "ENCL") {
+        totalMinted = 0;
+    }
+
+    function mint(address recipient, uint256 amount) external {
+        require(totalMinted + amount <= TOTAL_SUPPLY, "Exceeds cap");
+        _mint(recipient, amount);
+        totalMinted += amount;
+    }
 }
