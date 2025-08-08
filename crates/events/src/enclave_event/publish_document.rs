61ac8b623e03c2105502e43b7d57089b1426be8f
ryardley <contact@rudiyardley.com>
2025-08-08 11:46:28 +0200

    feat: license header

--- crates/events/src/enclave_event/publish_document.rs
+++ crates/events/src/enclave_event/publish_document.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use actix::Message;
 use chrono::{serde::ts_seconds, DateTime, Utc};
 use serde::{Deserialize, Serialize};
