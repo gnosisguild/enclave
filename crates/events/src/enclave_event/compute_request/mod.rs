6fc68efe84802562dd324f8d8e5cbf212f7f39cc
гλ <contact@rudiyardley.com>
2025-08-25 11:54:13 +0200

    feat: event definition (#612)
    * feat: event definition
    
    * feat: event definition with stages
    
    * feat: event definition for bfv events
    
    * feat: add error enum
    
    * feat: add spx header
    
    * feat: update names based on recommendations
    
    * feat: update names to be more consistent
    
    * feat: update rustdocs
    
    * feat: update rustdocs
    
    * feat: update rustdocs
    
    * feat: small refactor
    
    * feat: add errors
    
    * feat: update event params
    
    * feat: draft publish events
    
    * feat: license header
    
    * feat: add comments
    
    * feat: move events
    
    * feat: define document publisher functionality
    
    * feat: tighten up names
    
    * feat: add header
    
    * feat: fix version issue (#634)
    
    * feat: add logging
    
    * feat: try to use a fixed version of foundry
    
    * feat: try to use a more recent version
    
    * feat: revert version
    
    This reverts commit 12cd57a369dfb45c0a053dbc4a363c516f55e920.
    
    * feat: revert to see if outcome is the same
    
    * fix: remove / from zk-kit remapping
    
    ---------
    
    Co-authored-by: ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
    
    * feat: remove skip ci (#632)
    
    * feat: remove skip ci
    
    * feat: remove skip ci from if
    
    * chore: update valid commit types (#645)
    
    * docs(contributor): contributors readme action update (#644)
    
    * docs(contributor): contrib-readme-action has updated readme
    
    * Update README.md
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: гλ <contact@rudiyardley.com>
    
    * feat: update events to reflect changes
    
    * feat: update to include ids with shares
    
    * feat: update diagram
    
    * feat: update diagram
    
    * feat: update PartyId type
    
    * feat: use vec of smudging noise for creating decryption share
    
    * feat: use a vec instead of a single poly
    
    * feat: use a vec instead of a single poly
    
    * feat: remove readme
    
    * feat: update diagram to include pvw
    
    * feat: update diagram
    
    * feat: rename
    
    * feat: update trbfv_actor_seq.mmd
    
    * feat: update diagram
    
    ---------
    
    Co-authored-by: ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: ryardley <r@blockhackers.io>

---
+++ crates/events/src/enclave_event/compute_request/mod.rs
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
+pub mod bytes;
+pub mod trbfv;
+
+use crate::CorrelationId;
+use actix::Message;
+use serde::{Deserialize, Serialize};
+
+/// The compute instruction for a threadpool computation.
+/// This enum provides protocol disambiguation
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum ComputeRequest {
+    /// By Protocol
+    TrBFV(trbfv::TrBFVRequest),
+    // Eg. TFHE(TFHERequest)
+}
+
+/// The compute result from a threadpool computation
+/// This enum provides protocol disambiguation
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum ComputeResponse {
+    /// By Protocol
+    TrBFV(trbfv::TrBFVResponse),
+    // Eg. TFHE(TFHEResponse)
+}
+
+/// An error from a threadpool computation
+/// This enum provides protocol disambiguation
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum ComputeRequestError {
+    /// By Protocol
+    TrBFV(trbfv::TrBFVError),
+    // Eg. TFHE(TFHEError)
+}
+
+// Actix messages
+#[derive(Message, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[rtype(result = "()")]
+pub struct ComputeRequested {
+    correlation_id: CorrelationId,
+    request: ComputeRequest,
+}
+
+#[derive(Message, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[rtype(result = "()")]
+pub struct ComputeRequestFailed {
+    correlation_id: CorrelationId,
+    request: ComputeRequest,
+    error: ComputeRequestError,
+}
+
+#[derive(Message, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[rtype(result = "()")]
+pub struct ComputeRequestSucceeded {
+    correlation_id: CorrelationId,
+    response: ComputeResponse,
+}
