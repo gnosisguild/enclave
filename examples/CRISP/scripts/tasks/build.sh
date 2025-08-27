78ae3a42dc4188948afedaa43398f3f760ea9bdb
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-08-27 16:28:14 +0100

    chore: remove docker references in scripts

--- examples/CRISP/scripts/tasks/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-(cargo build --locked)
-(cd ./app/packages/evm && pnpm compile)
-(cd ./client && pnpm build)
--- examples/CRISP/scripts/test_e2e.sh
+++ examples/CRISP/scripts/test_e2e.sh
@@ -8,4 +8,4 @@ else
   PLAYWRIGHT_CMD="pnpm synpress --headless && xvfb-run pnpm playwright test"
 fi
 
-pnpm concurrently -krs first "pnpm dev:setup && pnpm dev:up" "wait-on http://localhost:3000 && ${PLAYWRIGHT_CMD} && sleep 3"
+concurrently -krs first "./scripts/setup.sh && ./scripts/dev.sh" "wait-on http://localhost:3000 && ${PLAYWRIGHT_CMD} && sleep 3"
