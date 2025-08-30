a8d846a29e6302b3ec81f1df97609503fcc79714
ryardley <r@blockhackers.io>
2025-08-30 08:13:10 +0000

    feat: update lock file and refactor conversions

--- crates/trbfv/src/gen_esi_sss.rs
+++ crates/trbfv/src/gen_esi_sss.rs
@@ -8,6 +8,7 @@ use crate::{ArcBytes, SharedRng, TrBFVConfig};
 use anyhow::{Context, Result};
 use e3_crypto::{Cipher, SensitiveBytes};
 use fhe_rs::trbfv::{smudging::SmudgingNoiseGenerator, ShareManager};
+use ndarray::Array2;
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 
@@ -21,17 +22,55 @@ pub struct Request {
     pub esi_per_ct: u64,
 }
 
+struct InnerRequest {
+    pub trbfv_config: TrBFVConfig,
+    pub error_size: BigUint,
+    pub esi_per_ct: u64,
+}
+
+impl TryFrom<Request> for InnerRequest {
+    type Error = anyhow::Error;
+    fn try_from(value: Request) -> std::result::Result<Self, Self::Error> {
+        Ok(InnerRequest {
+            trbfv_config: value.trbfv_config,
+            error_size: BigUint::from_bytes_be(&value.error_size),
+            esi_per_ct: value.esi_per_ct,
+        })
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Response {
     /// The smudging noise shares
     pub esi_sss: Vec<SensitiveBytes>,
 }
 
+impl TryFrom<(InnerResponse, &Cipher)> for Response {
+    type Error = anyhow::Error;
+    fn try_from(
+        (value, cipher): (InnerResponse, &Cipher),
+    ) -> std::result::Result<Self, Self::Error> {
+        Ok(Response {
+            esi_sss: value
+                .esi_sss
+                .into_iter()
+                .map(|s| SensitiveBytes::new(bincode::serialize(&s)?, cipher))
+                .collect::<Result<_>>()?,
+        })
+    }
+}
+
+struct InnerResponse {
+    pub esi_sss: Vec<Vec<Array2<u64>>>,
+}
+
 pub async fn gen_esi_sss(rng: &SharedRng, cipher: &Cipher, req: Request) -> Result<Response> {
+    let req = InnerRequest::try_from(req)?;
+
     let params = req.trbfv_config.params();
     let threshold = req.trbfv_config.threshold() as usize;
     let num_ciphernodes = req.trbfv_config.num_parties() as usize;
-    let error_size = BigUint::from_bytes_be(&req.error_size);
+    let error_size = req.error_size;
     let esi_per_ct = req.esi_per_ct as usize;
     let esi_sss = (0..esi_per_ct)
         .map(|_| -> Result<_> {
@@ -51,12 +90,5 @@ pub async fn gen_esi_sss(rng: &SharedRng, cipher: &Cipher, req: Request) -> Resu
         })
         .collect::<Result<Vec<Vec<_>>>>()?;
 
-    let esi_sss_result = esi_sss
-        .into_iter()
-        .map(|s| SensitiveBytes::new(bincode::serialize(&s)?, &cipher))
-        .collect::<Result<_>>();
-
-    Ok(Response {
-        esi_sss: esi_sss_result?,
-    })
+    Ok(Response::try_from((InnerResponse { esi_sss }, cipher))?)
 }
--- crates/trbfv/src/gen_pk_share_and_sk_sss.rs
+++ crates/trbfv/src/gen_pk_share_and_sk_sss.rs
@@ -4,10 +4,10 @@
 // without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE.
 
-use crate::{ArcBytes, SharedRng, TrBFVConfig};
-use anyhow::Result;
 /// This module defines event payloads that will generate the public key share as well as the sk shamir secret shares to be distributed to other members of the committee.
 /// This has been separated from the esi setup in order to be able to take advantage of parallelism
+use crate::{ArcBytes, SharedRng, TrBFVConfig};
+use anyhow::Result;
 use e3_crypto::{Cipher, SensitiveBytes};
 use fhe_rs::{
     bfv::SecretKey,
@@ -15,6 +15,7 @@ use fhe_rs::{
     trbfv::ShareManager,
 };
 use fhe_traits::Serialize as FheSerialize;
+use ndarray::Array2;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
@@ -26,6 +27,23 @@ pub struct Request {
     pub crp: ArcBytes,
 }
 
+struct InnerRequest {
+    pub trbfv_config: TrBFVConfig,
+    pub crp: CommonRandomPoly,
+}
+
+impl TryFrom<Request> for InnerRequest {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Request) -> std::result::Result<Self, Self::Error> {
+        let crp = CommonRandomPoly::deserialize(&value.crp, &value.trbfv_config.params())?;
+        Ok(InnerRequest {
+            trbfv_config: value.trbfv_config,
+            crp,
+        })
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Response {
     /// PublicKey share for this node
@@ -34,13 +52,38 @@ pub struct Response {
     pub sk_sss: Vec<SensitiveBytes>,
 }
 
+impl TryFrom<(InnerResponse, &Cipher)> for Response {
+    type Error = anyhow::Error;
+    fn try_from(
+        (value, cipher): (InnerResponse, &Cipher),
+    ) -> std::result::Result<Self, Self::Error> {
+        let pk_share = Arc::new(value.pk_share.to_bytes());
+        let sk_sss = value
+            .sk_sss
+            .into_iter()
+            .map(|s| SensitiveBytes::new(bincode::serialize(&s)?, cipher))
+            .collect::<Result<_>>()?;
+
+        Ok(Response { pk_share, sk_sss })
+    }
+}
+
+struct InnerResponse {
+    /// PublicKey share for this node
+    pub pk_share: PublicKeyShare,
+    /// SecretKey Shamir Shares for other parties
+    pub sk_sss: Vec<Array2<u64>>,
+}
+
 pub async fn gen_pk_share_and_sk_sss(
     rng: &SharedRng,
     cipher: &Cipher,
     req: Request,
 ) -> Result<Response> {
+    let req = InnerRequest::try_from(req)?;
+
     let params = req.trbfv_config.params();
-    let crp = CommonRandomPoly::deserialize(&req.crp, &params)?;
+    let crp = req.crp;
     let threshold = req.trbfv_config.threshold();
     let num_ciphernodes = req.trbfv_config.num_parties();
 
@@ -55,13 +98,8 @@ pub async fn gen_pk_share_and_sk_sss(
     let sk_sss =
         { share_manager.generate_secret_shares_from_poly(sk_poly, &mut *rng.lock().unwrap())? };
 
-    let sk_sss_result = sk_sss
-        .into_iter()
-        .map(|s| SensitiveBytes::new(bincode::serialize(&s)?, &cipher))
-        .collect::<Result<_>>();
-
-    Ok(Response {
-        pk_share: Arc::new(pk_share.to_bytes()),
-        sk_sss: sk_sss_result?,
-    })
+    Ok(Response::try_from((
+        InnerResponse { pk_share, sk_sss },
+        cipher,
+    ))?)
 }
