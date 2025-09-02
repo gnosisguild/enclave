77b1854a5c8e3bd4447e904e2a48ef6ea5f6895e
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-09-02 13:25:58 +0100

    fix: rebase and ensure tests work

--- examples/CRISP/playwright.config.ts
+++ examples/CRISP/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
+        headless: true,
       },
     },
   ],
--- examples/CRISP/scripts/test_e2e.sh
+++ examples/CRISP/scripts/test_e2e.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "$1" == "--ui" ]; then
-  PLAYWRIGHT_CMD="cd apps/client && pnpm synpress && pnpm playwright test --ui"
+  PLAYWRIGHT_CMD="pnpm synpress && pnpm playwright test"
 else
-  PLAYWRIGHT_CMD="cd apps/client && pnpm synpress --headless && xvfb-run pnpm playwright test"
+  PLAYWRIGHT_CMD="pnpm synpress --headless && xvfb-run pnpm playwright test"
 fi
 
 concurrently -krs first "./scripts/setup.sh && ./scripts/dev.sh" "wait-on http://localhost:3000 && ${PLAYWRIGHT_CMD} && sleep 3"
