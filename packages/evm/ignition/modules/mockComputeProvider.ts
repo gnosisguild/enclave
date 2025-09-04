1a8acfb3f68e28f8bf08e5dbd466ff5dfc447839
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-09-04 21:40:24 +0100

    chore: rebase to main

--- packages/evm/ignition/modules/mockComputeProvider.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("MockComputeProvider", (m) => {
-  const mockComputeProvider = m.contract("MockComputeProvider");
-
-  return { mockComputeProvider };
-}) as any;
--- packages/evm/ignition/modules/mockInputValidator.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("MockInputValidator", (m) => {
-  const mockInputValidator = m.contract("MockInputValidator");
-
-  return { mockInputValidator };
-}) as any;
