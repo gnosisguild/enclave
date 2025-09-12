d0246927c15e195b716f4b1957d0abe5c1b8c154
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-09-12 21:42:05 +0100

    chore: 0.0.13-test

--- packages/enclave-contracts/scripts/index.ts
+++ packages/enclave-contracts/scripts/index.ts
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 export * from "./deployEnclave";
 export * from "./deployMocks";
 export * from "./utils";
