fc87e91db2cd6479cba7748bbe3e8aa294117c46
ryardley <r@blockhackers.io>
2025-09-15 20:49:36 +0000

    feat: add header

--- crates/trbfv/src/shares/share_set_collection.rs
+++ crates/trbfv/src/shares/share_set_collection.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use anyhow::{bail, Result};
 use e3_crypto::Cipher;
 use ndarray::Array2;
