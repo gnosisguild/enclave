bb2971aedf7f56249a410f01e8417385c9170610
Cedoor <me@cedoor.dev>
2025-09-17 14:04:55 +0100

    chore: remove base64 to uint8 array function

--- crates/wasm/init_web.js
+++ crates/wasm/init_web.js
@@ -31,15 +31,3 @@ export default async function initializeWasm(initParams) {
 
   return promise;
 }
-
-function base64ToUint8Array(base64) {
-  const binaryString = atob(base64);
-  const len = binaryString.length;
-  const bytes = new Uint8Array(len);
-
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-
-  return bytes;
-}
