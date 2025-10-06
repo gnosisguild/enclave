d3ecb8e6416703993e883eb774f56c106be527e7
Hamza Khalid <36852564+hmzakhalid@users.noreply.github.com>
2025-10-06 19:40:21 +0500

    chore: update zkvm to v3.0.0 (#791)
    * chore: update zkvm to v3.0.0
    
    * chore: update deployment script

--- examples/CRISP/contracts/ImageID.sol
+++ examples/CRISP/contracts/ImageID.sol
@@ -19,5 +19,8 @@
 pragma solidity ^0.8.20;
 
 library ImageID {
-    bytes32 public constant PROGRAM_ID = bytes32(0x23734b77b0f76e85623a88d7a82f24c34c94834f2501964ea123b7a2027013a2);
+    bytes32 public constant PROGRAM_ID =
+        bytes32(
+            0x42d3447cf62d4f2a53f3a836258883e4063202f94b53303ee37ecac0643d82f1
+        );
 }
