dc2e962f6287ee45d48e73a6ad660056ae6befd6
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-10-13 11:56:59 +0100

    chore: remove semaphore and cleanup crisp (#833)
    * chore: remove semaphore and cleanup crisp
    
    * chore: remove last semaphore mention

--- examples/CRISP/client/src/utils/proof-encoding.ts
+++ examples/CRISP/client/src/utils/proof-encoding.ts
@@ -4,43 +4,19 @@
 // without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE.
 
-import { hexToBytes, encodeAbiParameters, parseAbiParameters, bytesToHex } from 'viem';
-import { type SemaphoreNoirProof } from '@hashcloak/semaphore-noir-proof';
-
-const abi = parseAbiParameters(
-    '(uint256,uint256,uint256,uint256,uint256,bytes)'
-);
-
-export function encodeSemaphoreProof(
-    { merkleTreeDepth, merkleTreeRoot, nullifier, message, scope, proofBytes }: SemaphoreNoirProof
-): Uint8Array {
-    const hex = encodeAbiParameters(abi, [
-        [
-            BigInt(merkleTreeDepth),
-            BigInt(merkleTreeRoot),
-            BigInt(nullifier),
-            BigInt(message),
-            BigInt(scope),
-            bytesToHex(proofBytes),
-        ]
-    ]);
-
-    return hexToBytes(hex);
-}
+import { encodeAbiParameters, parseAbiParameters, bytesToHex } from 'viem';
 
 const crispAbi = parseAbiParameters(
-    '(bytes, bytes, bytes32[], bytes)'
+    '(bytes, bytes32[], bytes)'
 );
 
 export const encodeCrispInputs = (
-    semaphoreProof: Uint8Array,
     noirProof: Uint8Array,
     noirPublicInputs: string[],
     encryptedVote: Uint8Array
 ): string => {
     return encodeAbiParameters(crispAbi, [
         [
-            bytesToHex(semaphoreProof),
             bytesToHex(noirProof),
             noirPublicInputs.map(input => input as `0x${string}`),
             bytesToHex(encryptedVote)
