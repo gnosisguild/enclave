e9a8b9b42766a6c1500d6e8c92100326e4da8e1a
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-10-16 12:27:12 +0100

    feat: add functionality to encrypt a u64 vector (#853)
    Co-authored-by: гλ <contact@rudiyardley.com>

--- packages/enclave-sdk/tests/sdk.test.ts
+++ packages/enclave-sdk/tests/sdk.test.ts
@@ -46,7 +46,28 @@ describe("encryptNumber", () => {
       const value = await sdk.encryptNumberAndGenProof(1n, Uint8Array.from(buffer), demoCircuit as unknown as CompiledCircuit);
       
       expect(value).to.be.an.instanceof(Object);
-      expect(value.encryptedVote).to.be.an.instanceof(Uint8Array);
+      expect(value.encryptedData).to.be.an.instanceof(Uint8Array);
+      expect(value.proof).to.be.an.instanceOf(Object)
+    }, 9999999);
+
+    it("should encrypt a vecor of numbers without crashing in a node environent", async () => {
+      const buffer = await fs.readFile(
+        path.resolve(__dirname, "./fixtures/pubkey.bin"),
+      );
+      const value = await sdk.encryptVector(new BigUint64Array([1n, 2n]), Uint8Array.from(buffer));
+      expect(value).to.be.an.instanceof(Uint8Array);
+      expect(value.length).to.equal(27_674);
+    });
+
+    it("should encrypt a vector and generate a proof without crashing in a node environent", async () => {
+      const buffer = await fs.readFile(
+        path.resolve(__dirname, "./fixtures/pubkey.bin"),
+      );
+
+      const value = await sdk.encryptVectorAndGenProof(new BigUint64Array([1n, 2n]), Uint8Array.from(buffer), demoCircuit as unknown as CompiledCircuit);
+      
+      expect(value).to.be.an.instanceof(Object);
+      expect(value.encryptedData).to.be.an.instanceof(Uint8Array);
       expect(value.proof).to.be.an.instanceOf(Object)
     }, 9999999);
   });
