815a3866ef5f468c4104605def526ab7d1cfd29f
Cedoor <me@cedoor.dev>
2025-10-24 18:48:00 +0100

    chore: add licenses

--- examples/CRISP/crates/generator/src/ciphertext_addition.rs
+++ examples/CRISP/crates/generator/src/ciphertext_addition.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use bigint_poly::*;
 use fhe::bfv::BfvParameters;
 use fhe::bfv::Ciphertext;
--- examples/CRISP/crates/generator/src/serialization.rs
+++ examples/CRISP/crates/generator/src/serialization.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 //! Serialization module for crisp inputs data
 //!
 //! This module handles the serialization of inputs data to JSON format.
