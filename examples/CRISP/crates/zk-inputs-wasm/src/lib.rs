7f6ad2d06a17d43d69247408aafd77a3d65e8899
Cedoor <me@cedoor.dev>
2025-10-27 12:25:27 +0000

    chore: add missing licenses

--- examples/CRISP/crates/zk-inputs-wasm/src/lib.rs
+++ examples/CRISP/crates/zk-inputs-wasm/src/lib.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 //! JavaScript library with WASM bindings for CRISP ZK inputs generation.
 //!
 //! This crate provides JavaScript bindings for the CRISP ZK inputs generator using WASM.
