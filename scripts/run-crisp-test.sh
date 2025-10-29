930537cdb1fc7493b60a30c6b1a69f006843833a
ryardley <contact@rudiyardley.com>
2025-10-29 13:27:06 -0300

    use input to add --ui

--- scripts/run-crisp-test.sh
+++ scripts/run-crisp-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf * && git reset --hard HEAD && git submodule update --init --recursive && pnpm install && cargo build && cd examples/CRISP && pnpm test:e2e --ui
+rm -rf * && git reset --hard HEAD && git submodule update --init --recursive && pnpm install && cargo build && cd examples/CRISP && pnpm test:e2e "$@"
