51f6fa46221a5582745adafce2c8a07d41c372a8
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-10-31 10:36:28 +0000

    test: ecdsa + merkle root

--- examples/CRISP/packages/crisp-sdk/src/utils.ts
+++ examples/CRISP/packages/crisp-sdk/src/utils.ts
@@ -48,6 +48,7 @@ export const generateMerkleProof = (
   }
 
   const leaf = hashLeaf(address, balance.toString())
+  console.log("leaf hash", leaf);
 
   const index = leaves.findIndex((l) => l === leaf)
 
