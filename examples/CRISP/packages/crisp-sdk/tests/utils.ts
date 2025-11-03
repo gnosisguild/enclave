c13456e81e756e10b36657e3de6a9f34138d24ec
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-11-03 14:16:51 +0000

    chore: add missing license

--- examples/CRISP/packages/crisp-sdk/tests/utils.ts
+++ examples/CRISP/packages/crisp-sdk/tests/utils.ts
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 export function normalizeCoefficient(coeff: string): string {
     if (coeff.startsWith('0x')) {
         return BigInt(coeff).toString();
