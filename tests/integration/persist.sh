846d07459b5ae2f6c4da8cb117945afeed2a91b0
Hamza Khalid <Hamzakhalid829@gmail.com>
2025-11-05 20:50:07 +0500

    fix: add cn5 to the registry in persist test

--- tests/integration/persist.sh
+++ tests/integration/persist.sh
@@ -39,6 +39,9 @@ pnpm ciphernode:add --ciphernode-address $CIPHERNODE_ADDRESS_3 --network localho
 heading "Add ciphernode $CIPHERNODE_ADDRESS_4"
 pnpm ciphernode:add --ciphernode-address $CIPHERNODE_ADDRESS_4 --network localhost
 
+heading "Add ciphernode $CIPHERNODE_ADDRESS_5"
+pnpm ciphernode:add --ciphernode-address $CIPHERNODE_ADDRESS_5 --network localhost
+
 heading "Request Committee"
 
 ENCODED_PARAMS=0x$($SCRIPT_DIR/lib/pack_e3_params.sh \
