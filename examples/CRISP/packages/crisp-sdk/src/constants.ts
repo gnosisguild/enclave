d14826c8be20c72165c8f48898e6539cdfe89afe
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-11-06 18:25:58 +0000

    fix: crisp circuit validation of encoded vote (#973)

--- examples/CRISP/packages/crisp-sdk/src/constants.ts
+++ examples/CRISP/packages/crisp-sdk/src/constants.ts
@@ -14,13 +14,13 @@ export const CRISP_SERVER_STATE_LITE_ENDPOINT = 'state/lite'
  * Half the minimum degree needed to support the maxium vote value
  * If you change MAXIMUM_VOTE_VALUE, make sure to update this value too.
  */
-export const HALF_LARGEST_MINIMUM_DEGREE = 28;
+export const HALF_LARGEST_MINIMUM_DEGREE = 28
 
 /**
- * This is the maximum value for a vote (Yes or No). This is 2^28
+ * This is the maximum value for a vote (Yes or No). This is 2^28 - 1
  * The minimum degree that BFV should use is 56 (to accommodate both Yes and No votes)
  */
-export const MAXIMUM_VOTE_VALUE = BigInt(Math.pow(2, HALF_LARGEST_MINIMUM_DEGREE))
+export const MAXIMUM_VOTE_VALUE = BigInt(Math.pow(2, HALF_LARGEST_MINIMUM_DEGREE) - 1)
 
 /**
  * Default BFV parameters for the CRISP ZK inputs generator.
