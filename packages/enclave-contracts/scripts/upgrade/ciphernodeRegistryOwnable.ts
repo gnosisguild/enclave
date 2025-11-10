61cb42cead93f2b83736aad16bb3918d5183bda3
Hamza Khalid <Hamzakhalid829@gmail.com>
2025-11-10 18:12:06 +0500

    chore: fix headers

--- packages/enclave-contracts/scripts/upgrade/ciphernodeRegistryOwnable.ts
+++ packages/enclave-contracts/scripts/upgrade/ciphernodeRegistryOwnable.ts
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
 import hre from "hardhat";
 
 import { upgradeAndSaveCiphernodeRegistryOwnable } from "../deployAndSave/ciphernodeRegistryOwnable";
