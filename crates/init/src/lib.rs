3f47f66ec0abbf843885e73a26ceb36473a6bb37
ryardley <contact@rudiyardley.com>
2025-11-12 11:29:56 -0300

    get commit hash of template

--- crates/init/src/lib.rs
+++ crates/init/src/lib.rs
@@ -54,7 +54,16 @@ async fn install_enclave(cwd: &PathBuf, template: Option<String>, verbose: bool)
         })
         .await?;
 
-    spinner.complete_task("Template downloaded\n");
+    let commit_hash = spinner
+        .run("Getting commit hash...", || async {
+            git::get_commit_hash(TEMP_DIR).await
+        })
+        .await?;
+
+    spinner.complete_task(&format!(
+        "Template downloaded with commit hash '{}'\n",
+        commit_hash
+    ));
 
     spinner.update("Configuring template...".to_string()).await;
 
