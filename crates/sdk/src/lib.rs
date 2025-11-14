da8021e0e805816d9fd5431e6a011e4a7e391617
ryardley <r@blockhackers.io>
2025-11-14 17:55:26 +0000

    use feature flags for sdk

--- crates/sdk/src/lib.rs
+++ crates/sdk/src/lib.rs
@@ -4,6 +4,11 @@
 // without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE.
 
+#[cfg(feature = "bfv-helpers")]
 pub use e3_bfv_helpers as bfv_helpers;
+
+#[cfg(feature = "evm-helpers")]
 pub use e3_evm_helpers as evm_helpers;
+
+#[cfg(feature = "indexer")]
 pub use e3_indexer as indexer;
