83d65e0d1c4414e7e13621ceedd6e67c20020467
ryardley <contact@rudiyardley.com>
2025-11-18 11:35:34 -0300

    enable trbfv flag for crisp

--- examples/CRISP/scripts/dev_cipher.sh
+++ examples/CRISP/scripts/dev_cipher.sh
@@ -24,7 +24,7 @@ enclave wallet set --name cn5 --private-key "$PRIVATE_KEY_CN5"
 
 # using & instead of -d so that wait works below
 # TODO: add --experimental-trbfv after testing
-enclave nodes up -v &
+enclave nodes up -v --experimental-trbfv &
 
 sleep 2
 
