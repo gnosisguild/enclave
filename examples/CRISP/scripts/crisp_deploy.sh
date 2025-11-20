a4005eab6485f69559ef3469b60ebb4458b93bec
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-11-20 13:46:16 -0300

    chore: test with risc0

--- examples/CRISP/scripts/crisp_deploy.sh
+++ examples/CRISP/scripts/crisp_deploy.sh
@@ -9,4 +9,4 @@ export PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4
 cd packages/crisp-contracts
 
 pnpm clean:deployments --network localhost
-USE_MOCK_VERIFIER=true pnpm deploy:contracts:full --network localhost
+pnpm deploy:contracts:full --network localhost
