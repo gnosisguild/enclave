85db76651a28864081b50220a97313567fe5e88b
Cedoor <me@cedoor.dev>
2025-11-28 15:35:31 +0000

    refactor: add prettier plugin for solidity

--- examples/CRISP/packages/crisp-contracts/contracts/Mocks/RiscZeroGroth16Verifier.sol
+++ examples/CRISP/packages/crisp-contracts/contracts/Mocks/RiscZeroGroth16Verifier.sol
@@ -5,9 +5,9 @@
 // or FITNESS FOR A PARTICULAR PURPOSE.
 pragma solidity >=0.8.27;
 
-import {RiscZeroGroth16Verifier as RiscZero} from "risc0/groth16/RiscZeroGroth16Verifier.sol";
-import {ControlID} from "risc0/groth16/ControlID.sol";
+import { RiscZeroGroth16Verifier as RiscZero } from 'risc0/groth16/RiscZeroGroth16Verifier.sol';
+import { ControlID } from 'risc0/groth16/ControlID.sol';
 
 contract RiscZeroGroth16Verifier is RiscZero {
-    constructor() RiscZero(ControlID.CONTROL_ROOT, ControlID.BN254_CONTROL_ID) {}
+  constructor() RiscZero(ControlID.CONTROL_ROOT, ControlID.BN254_CONTROL_ID) {}
 }
