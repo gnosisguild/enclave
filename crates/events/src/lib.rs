c27fc40c3b138601207eca6286d53cac62b985a7
ryardley <r@blockhackers.io>
2025-11-30 18:26:39 +0000

    event_manager.rs -> bus_handle.rs

--- crates/events/src/lib.rs
+++ crates/events/src/lib.rs
@@ -4,11 +4,11 @@
 // without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE.
 
+mod bus_handle;
 mod correlation_id;
 mod e3id;
 mod enclave_event;
 mod event_id;
-mod event_manager;
 mod eventbus;
 mod eventbus_factory;
 mod ordered_set;
@@ -16,11 +16,11 @@ pub mod prelude;
 mod seed;
 mod traits;
 
+pub use bus_handle::*;
 pub use correlation_id::*;
 pub use e3id::*;
 pub use enclave_event::*;
 pub use event_id::*;
-pub use event_manager::*;
 pub use eventbus::*;
 pub use eventbus_factory::*;
 pub use ordered_set::*;
