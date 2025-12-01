9bf3db50284e73071b618f74e17823674ed269ea
Cedoor <me@cedoor.dev>
2025-12-01 16:18:57 +0000

    refactor: add prettier plugin for solidity (#1054)

--- crates/indexer/tests/fixtures/emit_logs.sol
+++ crates/indexer/tests/fixtures/emit_logs.sol
@@ -7,28 +7,28 @@
 pragma solidity >=0.4.24;
 
 contract EmitLogs {
-    event ValueChanged(address indexed author, uint256 count, string value);
-    event PublishMessage(string value);
+  event ValueChanged(address indexed author, uint256 count, string value);
+  event PublishMessage(string value);
 
-    string _value;
+  string _value;
 
-    uint256 count = 0;
+  uint256 count = 0;
 
-    constructor() {
-        _value = "";
-    }
+  constructor() {
+    _value = "";
+  }
 
-    function getValue() public view returns (string memory) {
-        return _value;
-    }
+  function getValue() public view returns (string memory) {
+    return _value;
+  }
 
-    function setValue(string memory value) public {
-        count++;
-        emit ValueChanged(msg.sender, count, value);
-        _value = value;
-    }
+  function setValue(string memory value) public {
+    count++;
+    emit ValueChanged(msg.sender, count, value);
+    _value = value;
+  }
 
-    function emitPublishMessage(string memory value) public {
-        emit PublishMessage(value);
-    }
+  function emitPublishMessage(string memory value) public {
+    emit PublishMessage(value);
+  }
 }
