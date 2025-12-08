ad998d770dee11645291c1600a3878d12db7dbce
ryardley <r@blockhackers.io>
2025-12-08 03:30:53 +0000

    refactor sled_db

--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -5,20 +5,24 @@
 // or FITNESS FOR A PARTICULAR PURPOSE.
 
 mod data_store;
+mod events;
 mod in_mem;
 mod into_key;
 mod persistable;
 mod repositories;
 mod repository;
+mod sled_db;
 mod sled_store;
 mod snapshot;
 mod traits;
 
 pub use data_store::*;
+pub use events::*;
 pub use in_mem::*;
 pub use into_key::IntoKey;
 pub use persistable::*;
 pub use repositories::*;
 pub use repository::*;
+pub use sled_db::*;
 pub use sled_store::*;
 pub use snapshot::*;
