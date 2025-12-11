115e38adb95a61455184c28ab12adec49a47497f
ryardley <r@blockhackers.io>
2025-12-11 03:20:25 +0000

    header

--- crates/data/src/commit_log_event_log.rs
+++ crates/data/src/commit_log_event_log.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-2.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use std::path::PathBuf;
 
 use anyhow::Result;
