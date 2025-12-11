b47404b8c42549b5f2d2d4b2458b3e3eb484cb3c
ryardley <r@blockhackers.io>
2025-12-11 03:20:25 +0000

    header

--- crates/data/src/sled_utils.rs
+++ crates/data/src/sled_utils.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use anyhow::{Context, Result};
 use once_cell::sync::Lazy;
 use sled::{Db, Tree};
