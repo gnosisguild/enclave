58541dfd9025b7dd73a21a30fd27488fc7ee5119
ryardley <r@blockhackers.io>
2025-12-11 03:21:40 +0000

    header

--- crates/ciphernode-builder/src/event_system.rs
+++ crates/ciphernode-builder/src/event_system.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use crate::get_enclave_event_bus;
 use actix::{Actor, Addr};
 use anyhow::Result;
