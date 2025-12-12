2997af0f431a19405ecbf495bb608eb5a450055e
ryardley <r@blockhackers.io>
2025-12-12 00:49:33 +0000

    use image tag based on commit

--- crates/net/tests/run.sh
+++ crates/net/tests/run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-
 set -e
-
 # Export env vars once for all docker compose commands
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 
-echo ""
-echo "Building docker image"
-echo ""
-docker build --network host -f ./Dockerfile -t p2p_test:latest ../../..
+# Get the current commit SHA
+export IMAGE_TAG=$(git rev-parse --short HEAD)
 
+echo ""
+echo "Building docker image (p2p_test:${IMAGE_TAG})"
+echo ""
+docker build --network host -f ./Dockerfile -t "p2p_test:${IMAGE_TAG}" ../../..
 echo ""
 echo "NETWORK TESTS"
 echo ""
