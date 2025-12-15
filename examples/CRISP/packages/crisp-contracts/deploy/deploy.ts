c998e8fd2af3fdc6392c68908b2a38c938465e6f
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2025-12-15 22:19:32 +0000

    chore: print out .env to copy paste contracts

--- examples/CRISP/packages/crisp-contracts/deploy/deploy.ts
+++ examples/CRISP/packages/crisp-contracts/deploy/deploy.ts
@@ -3,7 +3,7 @@
 // This file is provided WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY
 // or FITNESS FOR A PARTICULAR PURPOSE.
-import { deployEnclave, updateE3Config } from '@enclave-e3/contracts/scripts'
+import { deployEnclave, readDeploymentArgs, updateE3Config } from '@enclave-e3/contracts/scripts'
 import { deployCRISPContracts } from './crisp'
 import path from 'path'
 
@@ -30,6 +30,7 @@ export const deploy = async () => {
   const chain = hre.globalOptions.network
 
   const shouldDeployEnclave = Boolean(process.env.DEPLOY_ENCLAVE)
+  const shouldPrintEnv = Boolean(process.env.PRINT_ENV_VARS)
 
   if (shouldDeployEnclave) {
     await deployEnclave(true)
@@ -38,6 +39,18 @@ export const deploy = async () => {
 
   // this expects you to run it from CRISP's root
   updateE3Config(chain, path.join(__dirname, '..', '..', '..', 'enclave.config.yaml'), contractMapping)
+
+  if (shouldPrintEnv) {
+    const enclaveAddress = readDeploymentArgs('Enclave', chain)?.address
+    const tokenAddress = readDeploymentArgs('MockUSDC', chain)?.address
+    const programAddress = readDeploymentArgs('CRISPProgram', chain)?.address
+    const ciphernodeRegistryAddress = readDeploymentArgs('CiphernodeRegistryOwnable', chain)?.address
+
+    console.log('\nAdd these to your server .env')
+    console.log(
+      `ENCLAVE_ADDRESS=${enclaveAddress}\nFEE_TOKEN_ADDRESS=${tokenAddress}\nE3_PROGRAM_ADDRESS=${programAddress}\nCIPHERNODE_REGISTRY_ADDRESS=${ciphernodeRegistryAddress}`,
+    )
+  }
 }
 
 deploy().catch((err) => {
