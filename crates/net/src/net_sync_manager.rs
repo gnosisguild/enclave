d17ef8dc7438c058a99f9eaad3905023ae5fe83d
ryardley <r@blockhackers.io>
2025-12-19 07:43:26 +0000

    add header

--- crates/net/src/net_sync_manager.rs
+++ crates/net/src/net_sync_manager.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use actix::{Actor, Addr, AsyncContext, Handler, Recipient, ResponseFuture};
 use anyhow::{anyhow, bail, Result};
 use e3_events::{
