610b60cbaca3a2f350842cd9a54bd8c5f44dceb7
Cedoor <me@cedoor.dev>
2026-01-22 15:43:02 +0100

    chore: add license headers

--- crates/polynomial/benches/polynomial.rs
+++ crates/polynomial/benches/polynomial.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use num_bigint::BigInt;
 use num_traits::{One, Zero};
