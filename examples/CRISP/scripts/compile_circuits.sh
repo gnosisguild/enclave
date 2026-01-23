a30cb6b78a6a2270cc824ea3b04b2f4b8c6a2c3c
Cedoor <me@cedoor.dev>
2026-01-23 19:47:44 +0100

    feat: add recursive proof

--- examples/CRISP/scripts/compile_circuits.sh
+++ examples/CRISP/scripts/compile_circuits.sh
@@ -5,28 +5,40 @@ set -euo pipefail
 echo "noir"
 
 # Compile the circuit
-echo "Compiling Noir circuit..."
-if ! (cd circuits && nargo compile); then
-    echo "Error: Noir circuit compilation failed"
+echo "Compiling CRISP circuit..."
+if ! (cd circuits/bin/crisp && nargo compile); then
+    echo "Error: CRISP circuit compilation failed"
+    exit 1
+fi
+
+echo "Compiling GRECO circuit..."
+if ! (cd circuits/bin/greco && nargo compile); then
+    echo "Error: GRECO circuit compilation failed"
     exit 1
 fi
 
 # Generate the Verifier
-echo "Generating Verifier Key..."
-if ! bb write_vk -b circuits/target/*.json -o circuits/target --oracle_hash keccak; then
-    echo "Error: Failed to generate Verifier Key"
+echo "Generating CRISP Verifier Key..."
+if ! bb write_vk -b circuits/bin/crisp/target/crisp.json -o circuits/bin/crisp/target --oracle_hash keccak; then
+    echo "Error: Failed to generate CRISP Verifier Key"
+    exit 1
+fi
+
+echo "Generating GRECO Verifier Key..."
+if ! bb write_vk -b circuits/bin/greco/target/crisp_greco.json -o circuits/bin/greco/target --oracle_hash keccak; then
+    echo "Error: Failed to generate GRECO Verifier Key"
     exit 1
 fi
 
 echo "Generating Solidity Verifier..."
-if ! bb write_solidity_verifier -k circuits/target/vk -o circuits/target/CRISPVerifier.sol; then
+if ! bb write_solidity_verifier -k circuits/bin/crisp/target/vk -o circuits/bin/crisp/target/CRISPVerifier.sol; then
     echo "Error: Failed to generate Solidity Verifier"
     exit 1
 fi
 
 # Copy the Solidity Verifier to the contracts folder
 echo "Copying Solidity Verifier to contracts folder..."
-if ! cp circuits/target/CRISPVerifier.sol packages/crisp-contracts/contracts/CRISPVerifier.sol; then
+if ! cp circuits/bin/crisp/target/CRISPVerifier.sol packages/crisp-contracts/contracts/CRISPVerifier.sol; then
     echo "Error: Failed to copy Solidity Verifier to contracts folder"
     exit 1
 fi
