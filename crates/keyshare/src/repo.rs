1e7e481b3be358dbcc2b172552b1e82b47a0edec
Hamza Khalid <36852564+hmzakhalid@users.noreply.github.com>
2026-01-28 21:04:19 +0500

    chore: remove unused keyshare code [skip-line-limit] (#1199)

--- crates/keyshare/src/repo.rs
+++ crates/keyshare/src/repo.rs
@@ -10,16 +10,6 @@ use e3_events::E3id;
 
 use crate::ThresholdKeyshareState;
 
-pub trait KeyshareRepositoryFactory {
-    fn keyshare(&self, e3_id: &E3id) -> Repository<Vec<u8>>;
-}
-
-impl KeyshareRepositoryFactory for Repositories {
-    fn keyshare(&self, e3_id: &E3id) -> Repository<Vec<u8>> {
-        Repository::new(self.store.scope(StoreKeys::keyshare(e3_id)))
-    }
-}
-
 pub trait ThresholdKeyshareRepositoryFactory {
     fn threshold_keyshare(&self, e3_id: &E3id) -> Repository<ThresholdKeyshareState>;
 }
