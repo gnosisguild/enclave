5c4657f8789d2c43bf56938b57ec51fc04a93010
ryardley <r@blockhackers.io>
2026-02-03 08:15:50 +0000

    add headers

--- crates/utils/src/error.rs
+++ crates/utils/src/error.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use tracing::error;
 
 /// Formats panic errors so they are seen in logs clearly
