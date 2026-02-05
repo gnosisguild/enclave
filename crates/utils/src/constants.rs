15bb576633973b5d3f4ccae9ec217b63551cc59d
ryardley <r@blockhackers.io>
2026-02-05 09:50:11 +0000

    add headers and fix indexer delay

--- crates/utils/src/constants.rs
+++ crates/utils/src/constants.rs
@@ -1,3 +1,12 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
+// NOTE: Currently this is here as everything depends on utils. We could consider moving this
+// closer to the configuration if we need to make this dynamic or create a create just for this.
+
 // Max message
 pub const MAILBOX_LIMIT: usize = 256;
 pub const MAILBOX_LIMIT_LARGE: usize = 256 * 10;
