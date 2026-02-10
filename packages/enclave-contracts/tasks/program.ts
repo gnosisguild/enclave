77b21d4b83e9aa3bdb8c7f1e19089856709ab9b3
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2026-02-10 09:30:09 +0000

    chore: add license

--- packages/enclave-contracts/tasks/program.ts
+++ packages/enclave-contracts/tasks/program.ts
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 import { task } from "hardhat/config";
 import { ArgumentType } from "hardhat/types/arguments";
 import { MockE3Program__factory as E3ProgramFactory } from "../types";
