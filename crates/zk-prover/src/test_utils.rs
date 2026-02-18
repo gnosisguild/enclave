8cc413f194bdccda27bfb6c731d9022fe7a5d14c
ryardley <r@blockhackers.io>
2026-02-18 06:48:44 +0000

    explain why we need to use get_tmpdir()

--- crates/zk-prover/src/test_utils.rs
+++ crates/zk-prover/src/test_utils.rs
@@ -9,6 +9,9 @@ use std::{fs, path::Path};
 use anyhow::Result;
 use tempfile::TempDir;
 
+/// Get the tempdir within ./target/tmp. This is important since some virtual environments such as nix
+/// won't necessarily have access to bb globaly. Not all tmp operations need to use this path only
+/// operations that require tools to exist within a shell at that location.
 pub fn get_tempdir() -> Result<TempDir> {
     let tmp = Path::new(env!("CARGO_MANIFEST_DIR"))
         .parent()
