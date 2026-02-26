1ec5d44a0f74e75b399642e38c8de8f456fab6a6
ryardley <r@blockhackers.io>
2026-02-26 06:04:09 +0000

    headers

--- crates/net/src/direct_responder.rs
+++ crates/net/src/direct_responder.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This file is provided WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.
+
 use crate::events::{IncomingResponse, NetCommand, ProtocolResponse, ProtocolResponseChannel};
 use anyhow::{anyhow, Context, Result};
 use e3_utils::OnceTake;
