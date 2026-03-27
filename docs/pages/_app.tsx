1c299bbc373c0af972b6dfa542529c7ea35568dc
0xjei <hello@0xjei.dev>
2026-03-27 16:27:07 +0100

    update docs with cryptography

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -6,6 +6,7 @@
 
 import React from 'react'
 import type { AppProps } from 'next/app'
+import 'katex/dist/katex.min.css'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
